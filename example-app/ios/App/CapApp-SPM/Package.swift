f75caba1ef7bfa81ee489caf6570248aa1d2a69b
EMI-INDO <cordova.c3addon@gmail.com>
2026-08-13 00:30:46 +0000

    GMA Next-Gen SDK: 1.3.1, Fix bug: Banner Race Condition

--- example-app/ios/App/CapApp-SPM/Package.swift
+++ example-app/ios/App/CapApp-SPM/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.1"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.4.2"),
         .package(name: "CapacitorSplashScreen", path: "../../../node_modules/@capacitor/splash-screen"),
         .package(name: "CapacitorAdmobNextgen", path: "../../../..")
     ],
